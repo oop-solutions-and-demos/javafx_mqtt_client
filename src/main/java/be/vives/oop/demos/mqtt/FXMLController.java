b6b10c5d0fb7135254f3789845ab658aceabac50
Nico De Witte <nico.dewitte@vives.be>
2019-03-27 12:51:00 +0100

    Create mqtt client and publish hello on button press

--- src/main/java/be/vives/oop/demos/mqtt/FXMLController.java
+++ src/main/java/be/vives/oop/demos/mqtt/FXMLController.java
@@ -6,20 +6,23 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import be.biosplanet.bioboost.mqtt.simple_mqtt_client.SimpleMQTTClient;
 
 public class FXMLController implements Initializable {
     
-    @FXML
-    private Label label;
+    @FXML private Label label;
     
+    private SimpleMQTTClient client = new SimpleMQTTClient();
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
+        client.publish("test/oop2/hello_world", "Hello World!");
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 }
